29397b8e22e588cec5ea52a0f4b387d2ede444bb
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-27 06:25:24 +0000

    config

--- basics/04_comparison.js
+++ basics/04_comparison.js
@@ -27,3 +27,5 @@ console.log(undefined < 0);    // => false
 /* strict check  (===)  */
 console.log("2"===2);  // => false, cause strict check also check data types.
 
+
+/* strict check  (===)  */
