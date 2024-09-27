96cd9c5b73e022a3a4d487ade0afe0ac95e84da7
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-27 05:54:10 +0000

    operations

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -27,4 +27,45 @@ let stringNumber = String(someNumber);
 // console.log(typeof stringNumber);
 
 
+//  ********************************** Operations ********************************** //
 
+let num = 3
+let negnum = -num
+// console.log(negnum);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " abhimanyu"
+
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");    // 
+// console.log("1" + 2 + 2);     // 122 , first agrument is string so all it will change all the arguments to string
+// console.log(1 + 2 + "2");      // 32  (1+2)=>3 , 2=>2 ==> 32
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);  // => 1
+// console.log(+"");    // => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;   // (postfix operation) "x:4, y:3"  , after assigning the value to b it will increment it self, cause const can't be modified
+
+let a = 3;
+const b = ++a;   // (prefix operation) "a:4, b:4"   , before assigning the value to b it will increment it self, cause const can't be modified
