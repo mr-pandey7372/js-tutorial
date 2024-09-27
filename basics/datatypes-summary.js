d89d807728dfb2f0b6d551037816d629b5dad2b9
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-27 08:16:07 +0000

    Memory Concept

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-//  Primitive 
+//  Primitive  :- Copy of the value
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
@@ -12,13 +12,13 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
  
-console.log(id === anotherId);  
+// console.log(id === anotherId);  
 
 const bigNumber = 34539872157893721n  // to make it big integer
 
 
 
-// Reference (Non- Primitive)
+// Reference (Non- Primitive) :- Reference to the same object
 
 // types: Array, Objects, Functions
 
@@ -37,5 +37,36 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
  
+
+// ******************************************************** //
+// ************************ Memory ************************ //
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+
+// ************************ Stack (Primitive) ************************ //
+let myYoutubename = "Abhimanyu.com"
+
+let anotherName = myYoutubename
+anotherName = "chai Aur Code"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+
+
+// ************************ Heap (Non-Primitive) ************************ //
+let userOne = {
+    email: "user.google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "abhi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
