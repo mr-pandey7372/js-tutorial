95776fed36bc66740ad21388f455f225871ae8e3
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-28 08:33:21 +0000

    updated comments in string

--- basics/05_string.js
+++ basics/05_string.js
@@ -3,7 +3,7 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);   // => follow 'backtick' to write the code 
 
 const gameName = new String('Abhimanyu-Pandey-ueiagdh')
 
