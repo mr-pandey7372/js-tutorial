1e1a0d2c41349434a5e90557c4bd14e9e8f2a850
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-28 08:34:19 +0000

    removed the comments

--- basics/05_string.js
+++ basics/05_string.js
@@ -1,27 +1,27 @@
 const name = "Abhimanyu"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+console.log(name + repoCount + " Value");
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);   // => follow 'backtick' to write the code 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);   // => follow 'backtick' to write the code 
 
 const gameName = new String('Abhimanyu-Pandey-ueiagdh')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
 
 // ******************************* Methods ******************************* //
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(8));
-// console.log(gameName.indexOf('y'));
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(8));
+console.log(gameName.indexOf('y'));
 
 const newString = gameName.substring(0,4)
-// console.log(newString);
+console.log(newString);
 
 const anotherString = gameName.slice(-9,4)   // start from back, we can pass negative values too.
-// console.log(anotherString);     
+console.log(anotherString);     
 
 const newStringOne = "   Abhimanyu    "
 console.log(newStringOne);       // =>     Abhimanyu     
