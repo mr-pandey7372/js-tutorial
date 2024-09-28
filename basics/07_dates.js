11af8f1be6b91b0fd39282760d19f50fc8499ad2
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-28 16:52:43 +0000

    date and time

---
+++ basics/07_dates.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+
+console.log(typeof myDate);    // object
+
+// self given date
+ 
+// let myCreatedDate = new Date(2024, 0, 23)    // (year, month, date)
+
+// let myCreatedDate = new Date(2024, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2024-01-14")
+let myCreatedDate = new Date("01-14-2024")
+
+console.log(myCreatedDate.toLocaleString());
+
