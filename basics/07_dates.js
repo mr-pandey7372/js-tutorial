b02109d98a403db9c02bd9bcb766e58441f8ab25
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-09-29 06:52:14 +0000

    updated time series

--- basics/07_dates.js
+++ basics/07_dates.js
@@ -9,7 +9,7 @@ let myDate = new Date()
 // console.log(myDate.toJSON());
 // console.log(myDate.toLocaleDateString());
 
-console.log(typeof myDate);    // object
+// console.log(typeof myDate);    // object
 
 // self given date
  
@@ -19,5 +19,20 @@ console.log(typeof myDate);    // object
 // let myCreatedDate = new Date("2024-01-14")
 let myCreatedDate = new Date("01-14-2024")
 
-console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleString());
 
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);  // => gives ms time stamp
+// console.log(myCreatedDate.getTime());   // convert the object time into ms time stamp
+
+// console.log(Math.floor(Date.now()/1000));   // converted into seconds time stamp
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",                            // "ctrl + space" for suggestion
+})
