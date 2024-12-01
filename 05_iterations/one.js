11b9866c7ed953891e106081fce36a2a6c3592c7
mr-pandey7372 <109463397+mr-pandey7372@users.noreply.github.com>
2024-12-01 08:08:52 +0000

    modified foor loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,13 +1,13 @@
 // for loop
 
-// for (let i = 0; i < 10; i++) {
-//     const element = i;
-//     if(element == 5){
-//         console.log("5 is the best number");
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        // console.log("5 is the best number");
         
-//     }
-//     console.log(element); 
-// }
+    }
+    // console.log(element); 
+}
 
 for (let i = 1; i <= 10; i++) {
     // console.log(`outer loop value: ${i}`);
